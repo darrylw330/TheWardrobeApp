c381d8053ae5ef52162056e32cfccc400b57b2a0
darrylw330 <126805312+darrylw330@users.noreply.github.com>
2023-05-15 20:14:53 +0000

    updated upload_controller to save clothing item

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -4,6 +4,32 @@ class UploadController < ApplicationController
     render :index
   end
 
+  def insert_clothing
+    query_user_id = params[:clothing][:query_user_id]
+  
+    # Create a new instance of Clothing and assign the attributes
+    @clothing = Clothing.new(clothing_params)
+  
+    # Assign the user ID to the clothing item
+    @clothing.user_id = query_user_id
+  
+    if @clothing.save
+      # Clothing item successfully saved
+      redirect_to user_profile_path(query_user_id)
+    else
+      # Handle the case where saving the clothing item fails
+      # You can render a view or redirect to an error page
+      # For simplicity, let's render the upload view with an error message
+      render :upload, alert: 'Failed to save the clothing item.'
+    end
+  end
+
+  private
+
+  def clothing_params
+    params.require(:clothing).permit(:image, :query_brand, :query_user_id)
+  end
+
   def create
     # Logic to handle the image upload and save it in the database
     @clothing = Clothing.new(clothing_params)
@@ -20,4 +46,3 @@ class UploadController < ApplicationController
     params.require(:clothing).permit(:name, :image)
   end
 end
-
