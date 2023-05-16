b6d6a75d2572a14ef60ed8d32e5d868ba29d99ef
darrylw330 <126805312+darrylw330@users.noreply.github.com>
2023-05-16 14:10:02 +0000

    removed 'name' from 'users' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_203245) do
+ActiveRecord::Schema.define(version: 2023_05_16_140806) do
 
   create_table "clothings", force: :cascade do |t|
     t.integer "user_id"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2023_05_15_203245) do
     t.integer "outfits_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "image"
   end
 
