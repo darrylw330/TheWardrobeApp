c58da3b9d406b64f761b88c1405a2d6aa1a077fb
darrylw330 <126805312+darrylw330@users.noreply.github.com>
2023-05-16 19:02:10 +0000

    added sidebar.js file + _sidebar.html.erb + updated css. Still not hidden

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+  var sidepanel = document.getElementById("mySidepanel");
+  sidepanel.classList.toggle("open");
+}
+
+function closeNav() {
+  var sidepanel = document.getElementById("mySidepanel");
+  sidepanel.classList.remove("open");
+}
