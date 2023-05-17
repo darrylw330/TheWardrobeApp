4157dc1a7bb0e6ed45956d1983c7f391c09d83b8
darrylw330 <126805312+darrylw330@users.noreply.github.com>
2023-05-17 16:12:10 +0000

    deleted sidbar nav + updated to show images after upload

--- app/assets/javascripts/sidebar.js
+++
@@ -1,9 +0,0 @@
-function toggleNav() {
-  var sidepanel = document.getElementById("mySidepanel");
-  sidepanel.classList.toggle("open");
-}
-
-function closeNav() {
-  var sidepanel = document.getElementById("mySidepanel");
-  sidepanel.classList.remove("open");
-}
--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -15,4 +15,6 @@ class Clothing < ApplicationRecord
   belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
   has_many(:outfit_clothings, { :class_name => "OutfitClothing", :foreign_key => "clothing_id", :dependent => :destroy })
   # has_one_attached :image
+
+  mount_uploader :image, ImageUploader
 end
